586b893d8440b258e79fd606a11d3559e4071896
rojajullapalli <rojajules@gmail.com>
2021-08-25 13:31:15 +0530

    ADDED:LinkedList To Do Stack Operation

--- src/com/bridgelabz/list/StackAndQueue.java
+++ src/com/bridgelabz/list/StackAndQueue.java
@@ -1,7 +1,44 @@
 package com.bridgelabz.list;
 
 public class StackAndQueue {
+    Node top;
+
+    public void push(int x) {
+        Node newNode=new Node(x);
+        if(isEmpty()){
+            top = newNode;
+        }
+        else{
+            newNode.data = x;
+            newNode.next = top;
+            top = newNode;
+        }
+    }
+
+    public boolean isEmpty() {
+        return top == null;
+    }
+
+    public void display() {
+        if (top == null) {
+            System.out.printf("\nStack Underflow");
+        }
+        else {
+            Node temp = top;
+            while (temp != null) {
+                System.out.print(temp.data+ " ");
+                temp = temp.next;
+            }
+        }
+    }
+
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Stack and Queue Program");
+        StackAndQueue list=new StackAndQueue();
+        list.push(70);
+        list.push(30);
+        list.push(56);
+        System.out.println("The Stack elements are ");
+        list.display();
     }
 }
