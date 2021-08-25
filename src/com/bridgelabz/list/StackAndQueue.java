7d964cb4c479af1bcd8b6c3f080bc8bc06d353eb
rojajullapalli <rojajules@gmail.com>
2021-08-25 13:26:22 +0530

    Welcome Message

---
+++ src/com/bridgelabz/list/StackAndQueue.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.list;
+
+public class StackAndQueue {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Stack and Queue Program");
+    }
+}
